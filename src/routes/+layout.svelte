08a3d3355a1adaa43cd5323f4c512c0d009fe9a5
Pblo16 <pablo.hv.2002@gmail.com>
2025-11-03 11:22:37 -0600

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import "../app.css";
+  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+  import AppSidebar from "$lib/components/app-sidebar.svelte";
+
+  let { children } = $props();
+</script>
+
+<div class="bg-background">
+  <Sidebar.Provider class="min-h-dvh flex">
+    <AppSidebar />
+    <div class="w-full">
+      <header
+        class="border-4 border-red-500 w-full h-fit flex items-center gap-4"
+      >
+        <Sidebar.Trigger />
+        <h1 class="text-lg">Application</h1>
+      </header>
+      <main
+        class="flex border-4 border-blue-500 p-4 h-[calc(100dvh-3rem)] overflow-auto"
+      >
+        {@render children?.()}
+      </main>
+    </div>
+  </Sidebar.Provider>
+</div>
